b39c83d1aa2f50828fb7e1c12c0d24a45f183b35
yuewangg <yw.robotics@gmail.com>
2022-04-02 22:31:36 +0800

    new

--- bvtsdk/examples/file_sonar copy/file_sonar.cpp
+++ bvtsdk/examples/file_sonar copy/file_sonar.cpp
@@ -10,7 +10,7 @@
 
 #include <bvt_sdk.h>
 
-char DataFile[] = "../../data/swimmer.son";
+char DataFile[] = "../../data/20m.son";
 
 int main( int argc, char *argv[] )
 {
@@ -33,12 +33,9 @@ int main( int argc, char *argv[] )
 		printf("BVTSonar_Open: ret=%d\n", ret);
 		return 1;
 	}
-
-	// Make sure we have the right number of heads
 	int heads = -1;
 	BVTSonar_GetHeadCount(son, &heads);
 	printf("BVTSonar_GetHeadCount: %d\n", heads);
-
 
 	// Get the first head
 	BVTHead head = NULL;
@@ -54,104 +51,57 @@ int main( int argc, char *argv[] )
 	BVTHead_GetPingCount(head, &pings);
 	printf("BVTHead_GetPingCount: %d\n", pings);
 
-    // Check the min and max range in this file
-    float min_range, max_range;
-    BVTHead_GetMinimumRange(head, &min_range);
-    BVTHead_GetMaximumRange(head, &max_range);
-    printf("BVTHead_GetMinimumRange: %0.2f\n", min_range );
-    printf("BVTHead_GetMaximumRange: %0.2f\n", max_range );
-
     BVTImageGenerator ig = BVTImageGenerator_Create();
     BVTImageGenerator_SetHead(ig, head);
 
     // Now, get a ping!
 	BVTPing ping = NULL;
-	ret = BVTHead_GetPing(head, 0, &ping);
+	ret = BVTHead_GetPing(head, 200, &ping);
 	if( ret != 0 )
 	{
 		printf("BVTHead_GetPing: ret=%d\n", ret);
 		return 1;
 	}
 	
-	// Generate an image from the ping
-	BVTMagImage img;
-	ret = BVTImageGenerator_GetImageXY(ig, ping, &img);
+	// Generate an RangeProfile from the ping
+	BVTRangeProfile rgp;
+	
+	ret = BVTImageGenerator_GetRangeProfile(ig, ping, &rgp);
 	if( ret != 0 )
 	{
-		printf("BVTImageGenerator_GetImageXY: ret=%d\n", ret);
+		printf("BVTHead_GetRangeCount: ret=%d\n", ret);
 		return 1;
 	}
 
-	printf("\n");
-
-	/////////////////////////////////////////////////////////
-	
-	// Check the image height and width out
-	int height;
-	BVTMagImage_GetHeight(img, &height);
-	printf("BVTMagImage_GetHeight: %d\n", height);
-	int width;
-	BVTMagImage_GetWidth(img, &width);
-	printf("BVTMagImage_GetWidth: %d\n", width);
-	
-	// Save it to a PGM (PortableGreyMap)
-	ret = BVTMagImage_SavePGM(img, "img.pgm");
+	float * range;
+	unsigned short * intensity;
+	int * count;
+	ret = BVTRangeProfile_GetCount(rgp,count);
 	if( ret != 0 )
 	{
-		printf("BVTMagImage_SavePGM: ret=%d\n", ret);
+		printf("BVTHead_GetRangeCount: ret=%d\n", ret);
 		return 1;
 	}
 
-	/////////////////////////////////////////////////////////
-	
-	// Build a color mapper
-	BVTColorMapper mapper;
-	mapper = BVTColorMapper_Create();
-	if( mapper == NULL )
-	{
-		printf("BVTColorMapper_Create: failed\n");
-		return 1;
-	}
-	
-	// Load the bone colormap
-	ret = BVTColorMapper_Load(mapper, "bone.cmap");
+	BVTRangeProfile_GetIntensityValue(rgp,0,intensity);
+
+
+	ret = BVTRangeProfile_GetRangeValue(rgp,0,range);
 	if( ret != 0 )
 	{
-		printf("BVTColorMapper_Load: ret=%d\n", ret);
+		printf("BVTHead_GetRangeValue: ret=%d\n", ret);
 		return 1;
 	}
 
-	
-	// Perform the colormapping
-	BVTColorImage cimg;
-	ret = BVTColorMapper_MapImage(mapper, img, &cimg);
+	ret = BVTRangeProfile_GetBearingValue(rgp,0,range);
 	if( ret != 0 )
 	{
-		printf("BVTColorMapper_MapImage: ret=%d\n", ret);
+		printf("BVTHead_GetRangeValue: ret=%d\n", ret);
 		return 1;
 	}
-	printf("\n");
 	
-	/////////////////////////////////////////////////////////
-	// Check the image height and width out
-	BVTColorImage_GetHeight(cimg, &height);
-	printf("BVTColorImage_GetHeight: %d\n", height);
-	BVTColorImage_GetWidth(cimg, &width);
-	printf("BVTColorImage_GetWidth: %d\n", width);
-
-	
-	// Save it to a PPM (PortablePixMap)
-	ret = BVTColorImage_SavePPM(cimg, "cimg.ppm");
-	if( ret != 0 )
-	{
-		printf("BVTColorImage_SavePPM: ret=%d\n", ret);
-		return 1;
-	}
-
 	// Clean up
-	BVTColorImage_Destroy(cimg);
-	BVTMagImage_Destroy(img);
-	BVTColorMapper_Destroy(mapper);
+	BVTRangeProfile_Destroy(rgp);
 	BVTPing_Destroy(ping);
 	BVTSonar_Destroy(son);
 	return 0;
